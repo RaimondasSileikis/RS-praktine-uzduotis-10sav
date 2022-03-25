49761304a41a0964e3b9bc99103d6d311196a201
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-03-25 11:00:39 +0200

    submit for rewiew

--- app.js
+++ app.js
@@ -14,3 +14,74 @@ Sukurti klasęVaisius, kuris turi:savybędydisrand 5 - 25;savybęidrand 1000000 
  Priešpatalpinantvaisiųįgrauztukaiobjektą,vaisiusturibūti“prakąstas”,Vaisiusobjekte paleidžiantprakasti()metodą.
 */
 
+class Vaisius {
+
+constructor(){
+  this.dydis = this.rand(5, 25);
+  this.id = this.rand(1000000, 9999999);
+  this.prakastas = false;
+}
+
+prakasti(){
+this.prakastas = true;
+}
+rand(min, max) {         
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+}
+const vaisius1 = new Vaisius();
+vaisius1.prakasti()
+console.log(vaisius1);
+
+class Krepsys{
+
+  static vaisiai = [];
+
+  static pripildyti(){
+for (let i = 0; i < 20; i++) {
+ this.vaisiai.push(new Vaisius)
+}
+this.vaisiai.sort((a, b) => a.dydis + b.dydis)
+  }
+  static isimtiVaisiai = [];
+  static pirmasVaisius;
+  static isimti(){
+    this.pirmasVaisius = this.vaisiai.shift()
+    this.isimtiVaisiai.push(this.pirmasVaisius);
+  }
+
+static papildyti(){
+  if (this.vaisiai.length < 20) {
+    for (let i = 0; i < (21 - this.vaisiai.length); i++) {
+      this.vaisiai.push(new Vaisius)
+    }
+    this.vaisiai.sort((a, b) => a.dydis + b.dydis)
+  }
+}
+static prakastasVaisius;
+static prakasti(){
+  for (let i = 0; i < this.isimtiVaisiai.length; i++) {
+    if (this.isimtiVaisiai.prakastas != 'false') {
+    this.prakastasVaisius = this.isimtiVaisiai[i]
+    grauztukai.push(this.prakastasVaisius)
+    }
+    
+  }
+}
+constructor(){
+
+}
+
+}
+Krepsys.pripildyti()
+Krepsys.isimti()
+Krepsys.isimti()
+Krepsys.papildyti()
+console.log(Krepsys.vaisiai);
+console.log(Krepsys.isimtiVaisiai);
+
+const grauztukai = new Map();
+console.log(grauztukai);
